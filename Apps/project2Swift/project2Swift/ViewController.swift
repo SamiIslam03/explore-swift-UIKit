cbbec03fb972bbcb24bc9b9353ba0b970ba9c5fa
SamiIslam03 <samiul.islam03@northsouth.edu>
2023-07-14 18:20:21 +0600

    Add project2

--- Apps/project2Swift/project2Swift/ViewController.swift
+++ Apps/project2Swift/project2Swift/ViewController.swift
@@ -5,20 +5,22 @@
 //  Created by Sami Islam on 7/13/23.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var button1: UIButton!
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
-    
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
 
-    
-    override func viewDidLoad() {
+    override func viewDidLoad () {
         super.viewDidLoad()
-
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+                
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
         button3.layer.borderWidth = 1
@@ -27,15 +29,18 @@ class ViewController: UIViewController {
         button2.layer.borderColor = UIColor.lightGray.cgColor
         button3.layer.borderColor = UIColor.lightGray.cgColor
         
-        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
-        
         askQuestion()
     }
     
-    func askQuestion(action: UIAlertAction! = nil){
+    func askQuestion(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()

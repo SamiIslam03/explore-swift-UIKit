6d9f4b7c2e25ab3aa33d310dd9764af35208422d
SamiIslam03 <samiul.islam03@northsouth.edu>
2023-07-25 23:49:24 +0600

    add auto layout in code

---
+++ Apps/Project-6-B-Swift/Project-6-B-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project-6-B-Swift
+//
+//  Created by Sami Islam on 7/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

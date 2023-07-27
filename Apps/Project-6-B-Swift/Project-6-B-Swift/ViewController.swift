1e6a579027a5b2fa57e75dbffcd1b5d7fe6aee93
SamiIslam03 <samiul.islam03@northsouth.edu>
2023-07-27 23:36:15 +0600

    add and practice layout anchor and challenges

--- Apps/Project-6-B-Swift/Project-6-B-Swift/ViewController.swift
+++ Apps/Project-6-B-Swift/Project-6-B-Swift/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         label1.backgroundColor = .red
         label1.text = "THESE"
         label1.sizeToFit()
-         
+        
         let label2 = UILabel()
         label2.translatesAutoresizingMaskIntoConstraints = false
         label2.backgroundColor = .cyan
@@ -48,14 +48,27 @@ class ViewController: UIViewController {
         view.addSubview(label4)
         view.addSubview(label5)
         
-        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+        //        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+        //
+        //        for label in viewsDictionary.keys{
+        //            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+        //        }
+        //
+        //        var metrics = ["labelHeight": 88]
+        //        view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]->=10-|" , options: [], metrics: metrics, views: viewsDictionary))
+        var previous: UILabel?
         
-        for label in viewsDictionary.keys{
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+        for label in [label1, label2, label3, label4, label5] {
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+//            label.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+            previous = label
         }
-        
-        var metrics = ["labelHeight": 88]
-        view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]->=10-|" , options: [], metrics: metrics, views: viewsDictionary))
     }
 
 

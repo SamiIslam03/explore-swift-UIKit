1f327983caab07af10b1e6d8f8bc6729d107b85b
SamiIslam03 <samiul.islam03@northsouth.edu>
2023-08-02 00:19:00 +0600

    add project8 challenges and set up project9

---
+++ Apps/project-9-Swift/project-9-Swift/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  project7Swift
+//
+//  Created by Sami Islam on 7/28/23.
+//
+
+import Foundation
+
+struct Petition: Codable{
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Apps/project-9-Swift/project-9-Swift/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  project7Swift
+//
+//  Created by Sami Islam on 7/28/23.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}

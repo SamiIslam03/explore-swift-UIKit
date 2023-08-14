e86f38e22094f9d311d90d36145303ed6eb4b9db
SamiIslam03 <samiul.islam03@northsouth.edu>
2023-08-15 03:47:47 +0600

    add Transform: CGAffineTransform and practice

--- Apps/project15Swift/project15Swift/ViewController.swift
+++ Apps/project15Swift/project15Swift/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView = UIImageView(image: UIImage(named: "penguin"))
-        imageView.center = CGPoint(x: 512, y: 384)
+        imageView.center = CGPoint(x: 683, y: 512)
         view.addSubview(imageView)
     }
     @IBAction func tapped(_ sender: UIButton) {
@@ -24,10 +24,34 @@ class ViewController: UIViewController {
         UIView.animate(withDuration: 1, delay: 0, options: [],  animations: {
             switch self.currentAnimation {
             case 0:
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
                 break
+            case 1:
+                self.imageView.transform = .identity
+                break
+            case 2:
+                self.imageView.transform = CGAffineTransform(translationX: -256, y: -256)
+                break
+            case 3:
+                self.imageView.transform = .identity
+                break
+            case 4:
+                self.imageView.transform = CGAffineTransform(rotationAngle: .pi)
+                break
+            case 5:
+                self.imageView.transform = .identity
+                break
+            case 6:
+                self.imageView.alpha = 0.1
+                self.imageView.backgroundColor = .green
+                break
+            case 7:
+                self.imageView.alpha = 1
+                self.imageView.backgroundColor = .clear
             default:
                 break
             }
+            
         }) { finished in
             sender.isHidden = false
         }

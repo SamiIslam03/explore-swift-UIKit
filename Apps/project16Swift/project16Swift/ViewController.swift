994d4fa099aed37ed9aa5cdc2cd79e82781ef630
SamiIslam03 <samiul.islam03@northsouth.edu>
2023-08-15 23:41:25 +0600

    add project 16 challenge-1 and project 17 set up.

--- Apps/project16Swift/project16Swift/ViewController.swift
+++ Apps/project16Swift/project16Swift/ViewController.swift
@@ -11,9 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var mapView: MKMapView!
     
-    override func viewDidLoad() {
+    override func viewDidLoad(){
         super.viewDidLoad()
-
+        
         let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2021 Summer Olympiad")
         let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
         let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light.")
@@ -21,9 +21,14 @@ class ViewController: UIViewController {
         let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
         
         mapView.addAnnotations([london, oslo, paris, rome, washington])
-
+        let identifier = "annotation"
+        
+        if let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView {
+            
+            return
+        }
+        
     }
-
 
 }
 
---
+++ Apps/project17Swift/project17Swift/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  project17Swift
+//
+//  Created by Sami Islam on 8/15/23.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}

5dd1b666e2a69064f20cb7b6e9269b42bb35f613
SamiIslam03 <samiul.islam03@northsouth.edu>
2023-08-15 15:06:37 +0600

    add Up and running with MapKit

---
+++ Apps/project16Swift/project16Swift/Capital.swift
@@ -0,0 +1,21 @@
+//
+//  Capital.swift
+//  project16Swift
+//
+//  Created by Sami Islam on 8/15/23.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String? = nil, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}

9e88e30b40277152af9eb90a4be2d8ff389e4239
SamiIslam03 <samiul.islam03@northsouth.edu>
2023-08-17 03:16:05 +0600

    add practice ofDebugging with assert()

--- Apps/project18Swift/project18Swift/ViewController.swift
+++ Apps/project18Swift/project18Swift/ViewController.swift
@@ -11,11 +11,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Some messages")
-        print(1, 2, 3, 4, 5, separator: "-")
-        print("Some message", terminator: " ")
+//        print("Some messages")
+//        print(1, 2, 3, 4, 5, separator: "-")
+//        print("Some message", terminator: " ")
+//        assert(1 == 1, "Math Failure!")
+//        assert(1 == 2, "Math Failure!")
+        assert(myReallySlowMethod() == true, "The slow method return false, which is a bad thing")
+        
     }
-
+    
+    func myReallySlowMethod () -> Bool {
+        return false
+    }
 
 }
 

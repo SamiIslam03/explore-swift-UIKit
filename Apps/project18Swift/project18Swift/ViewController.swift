21eb8d1ccec3d24621e23a73ce35e980e4062f84
SamiIslam03 <samiul.islam03@northsouth.edu>
2023-08-16 17:54:34 +0600

    add project 18 setup and learn Basic Swift debugging using print()

---
+++ Apps/project18Swift/project18Swift/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  project18Swift
+//
+//  Created by Sami Islam on 8/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Some messages")
+        print(1, 2, 3, 4, 5, separator: "-")
+        print("Some message", terminator: " ")
+    }
+
+
+}
+

ea659cafceb601a7038134d8e1ec57bd5678b8e2
SamiIslam03 <samiul.islam03@northsouth.edu>
2023-08-19 23:52:35 +0600

    add set up of project20

---
+++ Apps/project20Swift/project20Swift/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  project20Swift
+//
+//  Created by Sami Islam on 8/19/23.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+}

1528863c32dcea3dadb2e7bb14f0fd088b4ee977
SamiIslam03 <samiul.islam03@northsouth.edu>
2023-08-22 23:59:04 +0600

    add project22 set up

--- Apps/project21Swift/project21Swift/ViewController.swift
+++ Apps/project21Swift/project21Swift/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
     }
@@ -28,6 +28,7 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate{
             }
         }
     }
+    
     @objc func scheduleLocal() {
         registerCategories()
         
@@ -35,16 +36,16 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate{
         center.removeAllPendingNotificationRequests()
         
         let content = UNMutableNotificationContent()
-        content.title = "Late wake up call"
-        content.body = "The early bird catches the worm, but the second mouse gets the cheese."
-        content.categoryIdentifier = "alarm"
+        content.title = "Title goes here"
+        content.body = "Main text goes here"
+        content.categoryIdentifier = "customIdentifier"
         content.userInfo = ["customData": "fizzbuzz"]
         content.sound = UNNotificationSound.default
 
         var dateComponents = DateComponents()
         dateComponents.hour = 10
         dateComponents.minute = 30
-        //let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
 
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
@@ -60,6 +61,8 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate{
 
         center.setNotificationCategories([category])
     }
+    
+    
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         // pull out the buried userInfo dictionary
         let userInfo = response.notification.request.content.userInfo
---
+++ Apps/project22/project22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project22
+//
+//  Created by Sami Islam on 8/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

d18773ed4674d6e3370018cdcb59aa71e369f4b1
SamiIslam03 <samiul.islam03@northsouth.edu>
2023-08-23 23:55:20 +0600

    add Requesting location: Core Location

--- Apps/project22/project22/ViewController.swift
+++ Apps/project22/project22/ViewController.swift
@@ -5,15 +5,30 @@
 //  Created by Sami Islam on 8/22/23.
 //
 
+import CoreLocation
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var distanceReading: UILabel!
+    var locationManager: CLLocationManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+
+        view.backgroundColor = .gray
     }
 
-
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    // do stuff
+                }
+            }
+        }
+    }
 }
 

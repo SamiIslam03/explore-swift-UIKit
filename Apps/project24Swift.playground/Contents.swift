12a3eaf29d6c1a950fa8383c1ef7f51dfa3f7708
SamiIslam03 <samiul.islam03@northsouth.edu>
2023-08-28 16:59:35 +0600

    add project24 set up and practice some swift code

---
+++ Apps/project24Swift.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let name = "Taylor"
+
+//for letter in name {
+//    print("Give me a \(letter)!"
+//}
+//print(name[3])
+let letter = name[name.index(name.startIndex, offsetBy: 3)]
+extension String {
+    subscript(i: Int) -> String {
+        return String(self[index(startIndex, offsetBy: i)])
+    }
+}
+
+          

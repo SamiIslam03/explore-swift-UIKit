ea1ad641baf39b0f682c1aef0264a85d23734335
SamiIslam03 <samiul.islam03@northsouth.edu>
2023-08-30 23:42:16 +0600

    add set up or project 25 and navigation View controller

---
+++ Apps/project25Swift/project25Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project25Swift
+//
+//  Created by Sami Islam on 8/30/23.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

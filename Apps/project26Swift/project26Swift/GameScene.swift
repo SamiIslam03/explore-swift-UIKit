064f8da8253bc35c8f8e26696bf9b8646b29ac07
SamiIslam03 <samiul.islam03@northsouth.edu>
2023-09-01 22:14:14 +0600

    add set up of project26, a game

---
+++ Apps/project26Swift/project26Swift/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  project26Swift
+//
+//  Created by Sami Islam on 9/1/23.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+}

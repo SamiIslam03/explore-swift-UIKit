dd634eb2850291a1620c88881560a5bc49b1a423
SamiIslam03 <samiul.islam03@northsouth.edu>
2023-09-02 23:23:34 +0600

    add initial loadLevel function

--- Apps/project26Swift/project26Swift/GameScene.swift
+++ Apps/project26Swift/project26Swift/GameScene.swift
@@ -11,4 +11,25 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         
     }
+    func loadLevel() {
+        guard let levelURL = Bundle.main.url(forResource: "level1", withExtension: "txt") else { return }
+        guard let levelString = try? String(contentsOf: levelURL) else { return }
+        
+        let lines = levelString.components(separatedBy: "\n")
+        
+        for (row, line) in lines.reversed().enumerated() {
+            for (column, letter) in line.enumerated(){
+                let position = CGPoint(x: (64 * column) + 32, y: (64 * row + 32))
+                if letter == "x" {
+                    //load wall
+                } else if letter == "v" {
+                    //load vortex
+                } else if letter == "s" {
+                    //load star
+                } else if letter == "f" {
+                    //load finish point
+                } 
+            }
+        }
+    }
 }

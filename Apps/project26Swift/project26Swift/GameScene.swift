abf9c46a923978d88ad489926ccefb52e7c3956c
SamiIslam03 <samiul.islam03@northsouth.edu>
2023-09-03 03:10:00 +0600

    add Loading a level: categoryBitMask, collisionBitMask, contactTestBitMask and solved bug

--- Apps/project26Swift/project26Swift/GameScene.swift
+++ Apps/project26Swift/project26Swift/GameScene.swift
@@ -7,7 +7,7 @@
 
 import SpriteKit
 
-enum CollisionTypes: UInt32{
+enum CollisionTypes: UInt32 {
     case player = 1
     case wall = 2
     case star = 4
@@ -18,30 +18,30 @@ enum CollisionTypes: UInt32{
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-        
+        loadLevel()
     }
     func loadLevel() {
         guard let levelURL = Bundle.main.url(forResource: "level1", withExtension: "txt") else {
             fatalError("Could not find level1.txt in the app bundle.")
         }
-        guard let levelString = try? String(contentsOf: levelURL) else { fatalError("Could not find level1.txt in the app bundle.")
+        guard let levelString = try? String(contentsOf: levelURL) else {
+            fatalError("Could not load level1.txt from the app bundle.")
         }
-        
         let lines = levelString.components(separatedBy: "\n")
-        
+
         for (row, line) in lines.reversed().enumerated() {
-            for (column, letter) in line.enumerated(){
-                let position = CGPoint(x: (64 * column) + 32, y: (64 * row + 32))
+            for (column, letter) in line.enumerated() {
+                let position = CGPoint(x: (64 * column) + 32, y: (64 * row) + 32)
                 if letter == "x" {
                     //load wall
                     let node = SKSpriteNode(imageNamed: "block")
                     node.position = position
-
+                    
                     node.physicsBody = SKPhysicsBody(rectangleOf: node.size)
                     node.physicsBody?.categoryBitMask = CollisionTypes.wall.rawValue
                     node.physicsBody?.isDynamic = false
                     addChild(node)
-                } else if letter == "v" {
+                } else if letter == "v"  {
                     //load vortex
                     let node = SKSpriteNode(imageNamed: "vortex")
                     node.name = "vortex"
@@ -53,7 +53,6 @@ class GameScene: SKScene {
                     node.physicsBody?.categoryBitMask = CollisionTypes.vortex.rawValue
                     node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
                     node.physicsBody?.collisionBitMask = 0
-                    
                     addChild(node)
                 } else if letter == "s" {
                     //load star
@@ -67,13 +66,13 @@ class GameScene: SKScene {
                     node.physicsBody?.collisionBitMask = 0
                     node.position = position
                     addChild(node)
-                } else if letter == "f" {
+                } else if letter == "f"  {
                     //load finish point
                     let node = SKSpriteNode(imageNamed: "finish")
                     node.name = "finish"
                     node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
                     node.physicsBody?.isDynamic = false
-
+                    
                     node.physicsBody?.categoryBitMask = CollisionTypes.finish.rawValue
                     node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
                     node.physicsBody?.collisionBitMask = 0

ab06e7c64ec06bbb2bff8537dcddc3d32ff78ff5
SamiIslam03 <samiul.islam03@northsouth.edu>
2023-09-04 19:16:06 +0600

    add createPlayer function

--- Apps/project26Swift/project26Swift/GameScene.swift
+++ Apps/project26Swift/project26Swift/GameScene.swift
@@ -16,6 +16,7 @@ enum CollisionTypes: UInt32 {
 }
 
 class GameScene: SKScene {
+    var player: SKSpriteNode!
     
     override func didMove(to view: SKView) {
         loadLevel()
@@ -85,5 +86,19 @@ class GameScene: SKScene {
                 }
             }
         }
+    }
+    
+    func createPlayer () {
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 96, y: 672)
+        
+        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width / 2)
+        player.physicsBody?.allowsRotation = false
+        player.physicsBody?.linearDamping = 0.5
+        
+        player.physicsBody?.categoryBitMask = CollisionTypes.player.rawValue
+        player.physicsBody?.contactTestBitMask = CollisionTypes.star.rawValue | CollisionTypes.vortex.rawValue | CollisionTypes.finish.rawValue
+        player.physicsBody?.collisionBitMask = CollisionTypes.wall.rawValue
+        addChild(player)
     }
 }

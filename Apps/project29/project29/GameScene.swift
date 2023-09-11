a4d1cb5aa5d6c2eeda597adaa3a7dea39b9649f3
SamiIslam03 <samiul.islam03@northsouth.edu>
2023-09-11 23:33:29 +0600

    add project29 set up

---
+++ Apps/project29/project29/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  project29
+//
+//  Created by Sami Islam on 9/11/23.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+}

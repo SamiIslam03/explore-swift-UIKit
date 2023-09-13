c578953a53cea2a6e085d3c3f77022a58cb741e4
SamiIslam03 <samiul.islam03@northsouth.edu>
2023-09-13 23:34:41 +0600

    add the label and button and connect with the gameViewController

--- Apps/project29/project29/GameViewController.swift
+++ Apps/project29/project29/GameViewController.swift
@@ -12,6 +12,12 @@ import GameplayKit
 class GameViewController: UIViewController {
     var currentGame: GameScene!
 
+    @IBOutlet var angleSlider: UISlider!
+    @IBOutlet var angleLabel: UILabel!
+    @IBOutlet var velocitySlider: UISlider!
+    @IBOutlet var velocityLabel: UILabel!
+    @IBOutlet var launchButton: UIButton!
+    @IBOutlet var playerNumber: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -45,5 +51,11 @@ class GameViewController: UIViewController {
 
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    @IBAction func angleChanged(_ sender: Any) {
+    }
+    @IBAction func velocityChanged(_ sender: Any) {
+    }
+    @IBAction func launch(_ sender: Any) {
     }
 }
